59b8888737702777db8222fb2df5ece49713bc44
mp4402 <mariopisquiy@ufm.edu>
2020-09-20 10:56:37 -0600

    Suma, Resta y Simbols Modificada
    Se realizó la modificación para poder realizar la suma y la resta, así como la activación de sus banderas según lo indicado en la comparación en el método Simbols

--- CPU/ALU.py
+++ CPU/ALU.py
@@ -3,21 +3,45 @@ class ALU(Integratedcircuit):
     zero = False
     Overflow = False
     Negative = False 
-    
+    Igual = False
+    Mayor = False
+    Menor = False
     def __init__ (self, Opcode,Input):
         self.Opcode = Opcode
         self.Input = Input
-        pass
+        
     
     def Add(self, R0, R1):
+        R1 = R0 + R1
+        self.Simbols(R1,15,">")
+        if (self.Mayor == True):
+            self.Overflow = True
+            if (R1 % 2 == 0):
+                R1 = 14
+            else:
+                R1 = 15
+        self.Simbols(R1,0,"<")
+        if (self.Menor == True):
+            self.Negative == True
+        self.Simbols(R1,0,"==")
+        if(self.Igual == True):
+            self.zero = True
         # var = R0 + R1 (Si me da más de 15, veo si es par o impar y lo trunco)
         # R0 = var (si es par, 14.)
         # overflow lo vuelvo  true. lo imprimo. luego otra vez false. eso con todo. 
         # si la suma da más de 15, si el número es par, lo truncamos a 14 y si es impar lo truncamos a 15
-        pass
-    def Subtraction(self, R0, R1): # llamo a esta si R0 es más grande que R1, sino llamo a Borrow
+        
+    def Subtraction(self, R0, R1): 
+        R1 = R0- R1
+        self.simbols(R1,0,"==")
+        if(self.Igual == True):
+            self.zero = True
+        self.simbols(R1,0,"<")
+        if(self.menor == True):
+            self.Negative = True
+        # llamo a esta si R0 es más grande que R1, sino llamo a Borrow
         # si la resta es 0, la variables Zero la vuelvo True
-        pass
+        
     def Subtract_with_borrow(self):
         pass
     def Ocomplement(self):
@@ -31,4 +55,27 @@ class ALU(Integratedcircuit):
     def shift(self):
         pass
     def Simbols(self, R1, R0, simbolo):
+        Mayor = False
+        Menor = False
+        Igual = False
+        if (simbolo == ">"):
+            if(R1 > R0):
+                Mayor = True
+        elif (simbolo == "<"):
+            if(R1 < R0):
+                Menor = True
+        elif (simbolo == "=="):
+            if(R1 == R0):
+                Igual = True
+        elif (simbolo == ">="):
+            if(R1 >= R0):
+                Mayor = True
+                Igual = True
+        elif (simbolo == "<="):
+            if (R1 <= R0):
+                Menor = True
+                Igual = True
+        elif (simbolo == "!="):
+            if(R1 != R0):
+                Igual = False
         pass
