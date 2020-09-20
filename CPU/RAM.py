001b7a1ac9ec2074b25649dca7826df7134d3b87
mp4402 <mariopisquiy@ufm.edu>
2020-09-20 11:48:15 -0600

    Lectura de Archivo yaml

--- CPU/RAM.py
+++ CPU/RAM.py
@@ -1,7 +1,20 @@
+import yaml  
 class RAM:
     def __init__ (self):
+        with open("RAM.yaml","r") as ymlfile:
+            cfg = yaml.load(ymlfile)
+
+        for section in cfg:
+            print(section)
+        print(cfg["Visualization"])
         #leo el archivo y guardo en arrays la data y las instrucction. Los bios.yaml
         # self.RData = lo que está en data del archivo.
+        pass
+    hola = RAM()
+    def valorData(valor):
+        pass
+
+    def insertarValor(valor, data):
         pass
     # una funcion que retorne el valor de data: valorData(data)
     # Store_R0: En el array RData 
