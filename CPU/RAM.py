2a40edec9652fd668267e780c926129780f5350a
mp4402 <mariopisquiy@ufm.edu>
2020-09-21 15:54:05 -0600

    Actualización de la RAM

--- CPU/RAM.py
+++ CPU/RAM.py
@@ -1,15 +1,16 @@
 import yaml 
+import ROM
 data = [] 
 class RAM:
     def __init__ (self):
-        with open("RAM.yaml","r") as ymlfile:
+        with open("BIOS.yaml","r") as ymlfile:
             self.cfg = yaml.load(ymlfile)
 
         #for section in self.cfg:
          #   print(section)
         #print(self.cfg["visualization"])
-        self.clock = self.cfg["clock"]
-        self.visualizacion = self.cfg["visualization"]
+        #self.clock = self.cfg["clock"]
+        #self.visualizacion = self.cfg["visualization"]
         self.ram_ = self.cfg["RAM"]
         self.data = self.ram_.get("data")
         self.instruction = self.ram_.get("instructions")
@@ -22,12 +23,17 @@ class RAM:
         # self.RData = lo que está en data del archivo.
         pass
     #hola = RAM()
+    def cargarInstruction(self):
+        #Método usado para cargar las instrucciones en la RAM
+        self.instruction = ROM.Rom_array
+    
     def valorData(self, valor):
+        #Método usado para extraer un valor de la dada
         valor = int(valor)
         return self.data[valor]
-        pass
 
     def insertarValor(self,valor, data):
-        pass
+        #Método usado para escribir un valor en la data
+        self.data[valor] = data
     # una funcion que retorne el valor de data: valorData(data)
     # Store_R0: En el array RData 
