f0bed7093f02b89a6411d61f6c3969fd12e0d5ab
Javier Mazariegos <javiermazariegos@ufm.edu>
2020-09-22 00:16:02 -0600

    Creación función getdata

--- CPU/ROM.py
+++ CPU/ROM.py
@@ -34,6 +34,9 @@ class ROM:
     def getram(self):
         pass
 
+    def getdata(self):
+        return self.data
+
     def BuscarInstru(self, buscar, R0, R1, data):
         instruc = ""
         for i in range(len(self.Rom_array)):
